037f5276900c3b6030bf5dd26cb168c59a1beaae
Rudigus <rodrigo.matos9@hotmail.com>
2021-12-17 18:59:19 -0300

    adds detailed trait info

--- js/controller.js
+++ js/controller.js
@@ -5,6 +5,27 @@ const modalContent = document.getElementById("pet-modal-content");
 const span = document.getElementsByClassName("close")[0];
 var imageSource;
 
+setupModal();
+
+function setupModal() {
+    span.onclick = function () {
+        modal.style.display = "none";
+        // Removes everything from modal content, except the close button
+        for (var i = 1; i < modalContent.children.length; i) {
+            modalContent.removeChild(modalContent.children[i]);
+        }
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            // Removes everything from modal content, except the close button
+            for (var i = 1; i < modalContent.children.length; i) {
+                modalContent.removeChild(modalContent.children[i]);
+            }
+        }
+    }
+}
+
 function setImageSource() {
     for (var i = 0; i < imageSources.length; i++) {
         if (imageSources[i].checked) {
@@ -26,15 +47,30 @@ function getPetImage(petImageURL) {
 
 function populateTokenListing(pets) {
     petListing.replaceChildren();
-    // Owned Trait Count
-    const ownedTraitCount = document.createElement("label");
-    ownedTraitCount.innerText = `${getOwnedTraitCount(pets)} Owned Traits`;
-    ownedTraitCount.style.display = "block";
-    ownedTraitCount.style.maxWidth = "200px";
-    ownedTraitCount.style.margin = "15px auto";
-    ownedTraitCount.classList.add("rounded-rectangle");
-    ownedTraitCount.style.backgroundColor = "#A52A2A";
-    petListing.appendChild(ownedTraitCount);
+    loadOwnedTraits(pets, function () {
+        // Unique Trait Count
+        const uniqueTraitCount = document.createElement("label");
+        uniqueTraitCount.innerText = ownedTraits.stats.uniqueTraitCount + "/" +
+            totalTraits.totals.totalAttr + " Unique Traits";
+        uniqueTraitCount.style.fontSize = "1.25rem";
+        uniqueTraitCount.style.display = "block";
+        uniqueTraitCount.style.maxWidth = "200px";
+        uniqueTraitCount.style.margin = "15px auto";
+        petListing.appendChild(uniqueTraitCount);
+        // Show Detailed Info
+        const showDetailedInfo = document.createElement("label");
+        showDetailedInfo.innerText = "Show Detailed Info";
+        showDetailedInfo.style.display = "block";
+        showDetailedInfo.style.cursor = "pointer";
+        showDetailedInfo.style.maxWidth = "200px";
+        showDetailedInfo.style.margin = "15px auto";
+        showDetailedInfo.classList.add("rounded-rectangle");
+        showDetailedInfo.style.backgroundColor = "#A52A2A";
+        showDetailedInfo.addEventListener("click", function () {
+            presentTraitModal();
+        });
+        petListing.appendChild(showDetailedInfo);
+    });
     pets.forEach(pet => {
         // Pet Image
         const petImage = document.createElement("img");
@@ -85,4 +121,57 @@ function populateTokenListing(pets) {
         petListing.appendChild(petCard);
     });
     hideSearchLoader();
+}
+
+function presentTraitModal() {
+    for (var trait in ownedTraits) {
+        if (trait == "stats") {
+            continue;
+        }
+        traitLabel = document.createElement('label');
+        traitLabel.innerText = trait;
+        traitLabel.style.display = "block";
+        traitLabel.style.color = "black";
+        traitLabel.style.fontSize = "1.25rem";
+        traitLabel.style.textAlign = "center";
+        traitLabel.style.margin = "30px 0 10px";
+        // Trait Details Table
+        traitDetails = document.createElement('table');
+        traitDetails.style.width = '100px';
+        traitDetails.style.border = '1px solid black';
+        traitDetails.style.margin = "0 auto";
+        traitDetails.classList.add("styled-table");
+
+        let headerNames = ["Trait", "Owned", "Available", "Ownership"];
+        const tableRow = traitDetails.insertRow();
+        headerNames.forEach(headerName => {
+            let headerCell = document.createElement("th");
+            headerCell.innerText = headerName;
+            tableRow.appendChild(headerCell);
+        });
+        let sortedKeys = Object.keys(ownedTraits[trait]).sort((a, b) => {
+            return ownedTraits[trait][a] - ownedTraits[trait][b];
+        });
+        for (let i = 0; i < sortedKeys.length; i++) {
+            let key = sortedKeys[i];
+            const tableRow = traitDetails.insertRow();
+            let formattedTraitName = key;
+            let ownedTraitCount = ownedTraits[trait][key];
+            let traitName = Object.keys(totalTraits.attributes).find(element => {
+                return formatTraitName(element) == trait;
+            });
+            let availableTraitCount = totalTraits.attributes[traitName][key];
+            let ownership = ((ownedTraitCount / availableTraitCount) * 100).toFixed(2) + "%";
+            let cellData = [formattedTraitName, ownedTraitCount, availableTraitCount, ownership];
+            for (let j = 0; j < headerNames.length; j++) {
+                const tableDataCell = tableRow.insertCell();
+                tableDataCell.appendChild(document.createTextNode(cellData[j]));
+                tableDataCell.style.border = '1px solid black';
+            }
+        }
+        modalContent.appendChild(traitLabel);
+        modalContent.appendChild(traitDetails);
+    }
+
+    modal.style.display = "block";
 }
--- js/model.js
+++ js/model.js
@@ -1,5 +1,15 @@
 const policyID = "a5bb0e5bb275a573d744a021f9b3bff73595468e002755b447e01559";
 
+let ownedTraits = {
+    "stats": {
+        "traitCount": 0,
+        "uniqueTraitCount": 0
+    }
+};
+let totalTraits = {};
+
+loadTotalTraits();
+
 function loadAssets(walletAddress, callback) {
     var url = 'https://pool.pm/wallet/' + walletAddress;
     var xhr = new XMLHttpRequest();
@@ -22,29 +32,76 @@ function getRarityJSON(callback) {
     xhr.send();
 }
 
+function loadTotalTraits() {
+    var url = `https://tools-anywhere.herokuapp.com/https://cnft.tools/api/project/hoskyattr/`;
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        totalTraits = xhr.response;
+        // Removes the none traits from totalAttr
+        totalTraits.totals.totalAttr -= 6
+    };
+    xhr.responseType = "json";
+    xhr.open('GET', url);
+    xhr.send();
+}
+
 function getHoskyNumber(hosky) {
     let hoskyNumber = hosky.name.substring(13);
     hoskyNumber = parseInt(hoskyNumber, 10);
     return hoskyNumber;
 }
 
-function getOwnedTraitCount(hoskies) {
-    var ownedTraitCount = 0;
-    var ownedTraits = [];
+function formatTraitName(traitName) {
+    // Adds spaces between words (based on capital letters)
+    traitName = traitName.replace(/([A-Z])/g, ' $1').trim();
+    // Capitalizes the first letter
+    traitName = traitName.charAt(0).toUpperCase() + traitName.slice(1);
+    return traitName;
+}
+
+function loadOwnedTraits(hoskies, callback) {
+    // Resets
+    for (let key in ownedTraits) {
+        if (!ownedTraits.hasOwnProperty(key)) {
+            continue;
+        }
+        for (let subkey in key) {
+            if (ownedTraits[key].hasOwnProperty(subkey)) {
+                ownedTraits[key][subkey] = 0;
+            }
+        }
+    }
+    // Initializes
+    for (let trait in totalTraits.attributes) {
+        if (trait == "traitCount") {
+            continue;
+        }
+        formattedTrait = formatTraitName(trait);
+        if (!ownedTraits.hasOwnProperty(formattedTrait)) {
+            ownedTraits[formattedTrait] = {};
+        }
+        for (let key in totalTraits.attributes[trait]) {
+            ownedTraits[formattedTrait][key] = 0;
+        }
+    }
+    // Populates
     hoskies.forEach(hosky => {
         let traits = hosky["metadata"]["-----Traits-----"]
         for (let i = 0; i < traits.length; i++) {
             let trait = traits[i];
             for (var key in trait) {
                 if (!ownedTraits.hasOwnProperty(key)) {
-                    ownedTraits[key] = [];
+                    ownedTraits[key] = {};
                 }
-                if (trait.hasOwnProperty(key) && !ownedTraits[key].includes(trait[key])) {
-                    ownedTraits[key].push(trait[key]);
-                    ownedTraitCount += 1;
+                if (trait.hasOwnProperty(key) && ownedTraits[key].hasOwnProperty(trait[key])) {
+                    if (ownedTraits[key][trait[key]] == 0) {
+                        ownedTraits.stats.uniqueTraitCount += 1;
+                    }
+                    ownedTraits[key][trait[key]] += 1;
                 }
+                ownedTraits.stats.traitCount += 1;
             }
         }
     })
-    return ownedTraitCount;
+    callback();
 }
